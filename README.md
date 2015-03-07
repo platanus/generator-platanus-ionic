e8f5e89e901f2b834891da13ce61f9b1cbddceae
René A. Morales <rene.morales.sanchez@gmail.com>
2015-03-07 19:01:26 -0300

    feat(): first functional generator

--- README.md
+++ README.md
@@ -1,47 +1,23 @@
-# generator-platanus-ionic [![Build Status](https://secure.travis-ci.org/platanus/generator-platanus-ionic.png?branch=master)](https://travis-ci.org/platanus/generator-platanus-ionic)
+# Platanus Ionic Generator
 
-> [Yeoman](http://yeoman.io) generator
+Get up and running with Ionic, Crosswalk, Restmod, ngCordova, Karma and more.
 
+## Getting started
 
-## Getting Started
+- Install Yeoman.
+- Clone this repository and run `npm link` on its directory.
+- Now you can run `yo platanus-ionic` on an empty folder to start up the generator.
 
-### What is Yeoman?
+## What does it do?
 
-Trick question. It's not a thing. It's this guy:
+- Starts an empty Ionic project.
+- Sets up SCSS support.
+- Adds specified platforms and Crosswalk if necessary.
+- Sets up an API proxy.
+- Adds extra Bower components (angular-mocks, angular-restmod, ngCordova).
+- Adds extra npm packages (karma, karma-jasmine, karma-phantomjs-launcher).
+- Adds project files (.gitignore, karma.conf.js).
 
-![](http://i.imgur.com/JHaAlBJ.png)
+## After generating
 
-Basically, he wears a top hat, lives in your computer, and waits for you to tell him what kind of application you wish to create.
-
-Not every new computer comes with a Yeoman pre-installed. He lives in the [npm](https://npmjs.org) package repository. You only have to ask for him once, then he packs up and moves into your hard drive. *Make sure you clean up, he likes new and shiny things.*
-
-```bash
-npm install -g yo
-```
-
-### Yeoman Generators
-
-Yeoman travels light. He didn't pack any generators when he moved in. You can think of a generator like a plug-in. You get to choose what type of application you wish to create, such as a Backbone application or even a Chrome extension.
-
-To install generator-platanus-ionic from npm, run:
-
-```bash
-npm install -g generator-platanus-ionic
-```
-
-Finally, initiate the generator:
-
-```bash
-yo platanus-ionic
-```
-
-### Getting To Know Yeoman
-
-Yeoman has a heart of gold. He's a person with feelings and opinions, but he's very easy to work with. If you think he's too opinionated, he can be easily convinced.
-
-If you'd like to get to know Yeoman better and meet some of his friends, [Grunt](http://gruntjs.com) and [Bower](http://bower.io), check out the complete [Getting Started Guide](https://github.com/yeoman/yeoman/wiki/Getting-Started).
-
-
-## License
-
-MIT
+Use `ionic serve` to start a web server. Happy development!
--- app/index.js
+++ app/index.js
@@ -1,8 +1,14 @@
 'use strict';
+var fs = require('fs')
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var child_process = require('child_process');
+var exec = child_process.execSync;
+
+var ionic = function(args) {
+  return child_process.execFileSync('ionic', args.split(' '));
+};
 
 module.exports = yeoman.generators.Base.extend({
   initializing: function () {
@@ -20,28 +26,93 @@ module.exports = yeoman.generators.Base.extend({
     var prompts = [{
       type: 'input',
       name: 'appName',
-      message: 'What is the name of your new app?',
+      message: 'What is the name of this new app?',
       default: 'platanus-ionic-starter'
+    }, {
+      type: 'checkbox',
+      name: 'platforms',
+      message: 'What platforms do you need to support?',
+      choices: [
+        { name: 'iOS', value: 'ios' },
+        { name: 'Android', value: 'android' }
+      ]
+    }, {
+      type: 'confirm',
+      name: 'useCrosswalk',
+      message: 'Since Android is supported, should I include Crosswalk in the project?',
+      default: true,
+      when: function(answers){
+        if ( answers.platforms.indexOf('android') > -1 ) return true;
+        return false;
+      }
+    }, {
+      type: 'input',
+      name: 'proxyApiUrl',
+      message: 'Please enter the base API url that will be proxified through /api '+chalk.yellow('(e.g. http://myapp.pow/api, empty if not used)')
     }];
 
     this.prompt(prompts, function (answers) {
-      this.options.appName = answers.appName;
+      this.options = answers;
       done();
     }.bind(this));
   },
 
   writing: {
     ionicSetup: function() {
-      console.log('Initializing Ionic app (running '+chalk.yellow('ionic start')+')...');
-      child_process.execFileSync('ionic', ['start', this.options.appName, 'tabs']);
-      child_process.execSync('cp -rf ' + this.options.appName + '/. .');
-      child_process.execSync('rm -rf ' + this.options.appName);
+      console.log('Initializing Ionic app...');
+      ionic('start '+this.options.appName+' tabs');
+      exec('cp -rf ' + this.options.appName + '/. .');
+      exec('rm -rf ' + this.options.appName);
+    },
+    ionicSetupSass: function() {
+      console.log('Setting up SCSS support...');
+      ionic('setup sass');
+    },
+    ionicSetupProxy: function() {
+      if ( this.options.proxyApiUrl && this.options.proxyApiUrl.length > 0 ) {
+        console.log('Setting up API proxy...');
+        var ionicProjectFile = fs.readFileSync('ionic.project');
+        var ionicProject = JSON.parse(ionicProjectFile);
+        ionicProject.proxies = [
+          { 'path': '/api', 'proxyUrl': this.options.proxyApiUrl }
+        ];
+        fs.writeFileSync('ionic.project', JSON.stringify(ionicProject, null, 2));
+      }
+    },
+    ionicSetupPlatforms: function() {
+      if ( this.options.platforms.indexOf('ios') > -1 ) {
+        console.log('Adding iOS platform...');
+        ionic('platform add ios');
+      }
+      if ( this.options.platforms.indexOf('android') > -1 ) {
+        console.log('Adding Android platform...');
+        ionic('platform add android');
+      }
+    },
+    ionicSetupCrosswalk: function() {
+      if ( this.options.useCrosswalk ) {
+        console.log('Adding Crosswalk support...');
+        ionic('browser add crosswalk');
+      }
+    },
+    projectFiles: function() {
+      this.fs.copy(
+        this.templatePath('gitignore'),
+        this.destinationPath('.gitignore')
+      );
+      this.fs.copy(
+        this.templatePath('karma.conf.js'),
+        this.destinationPath('karma.conf.js')
+      );
     }
   },
 
   install: function () {
-    this.installDependencies({
-      skipInstall: this.options['skip-install']
-    });
+    console.log('Setting up dependencies...');
+
+    this.bowerInstall(['angular-mocks', 'angular-restmod', 'ngCordova'], { 'saveDev': true });
+    this.npmInstall(['karma', 'karma-jasmine', 'karma-phantomjs-launcher'], { 'saveDev': true });
+
+    this.installDependencies();
   }
 });
